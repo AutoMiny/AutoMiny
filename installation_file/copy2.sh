5dc5f840fc0229568e35f04a5c20272df2fc2112
AutoModelCar <zahra.boroujeni@fu-berlin.de>
2016-12-14 15:38:30 +0100

    Update copy2.sh

--- installation_file/copy2.sh
+++ installation_file/copy2.sh
@@ -1,9 +1,9 @@
-sudo scp -r model_car/catkin_ws root@192.168.1.199:./
-sudo scp -r model_car/extra_library/glfw root@192.168.1.199:./
-sudo scp -r model_car/extra_library/librealsense root@192.168.1.199:./
-sudo scp model_car/autostart.sh root@192.168.1.199:.//
-sudo scp model_car/.bashrc root@192.168.1.199:./
-sudo scp set.sh root@192.168.1.199:./
-sudo scp set2.sh root@192.168.1.199:./
+scp -r model_car/catkin_ws root@192.168.1.199:./
+scp -r model_car/extra_library/glfw root@192.168.1.199:./
+scp -r model_car/extra_library/librealsense root@192.168.1.199:./
+scp model_car/autostart.sh root@192.168.1.199:.//
+scp model_car/.bashrc root@192.168.1.199:./
+scp set.sh root@192.168.1.199:./
+scp set2.sh root@192.168.1.199:./
 
 echo "copy finished!"
