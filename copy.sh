85454cac3abbe760fc07a4a66a0fdf70ea9ce415
AutoModelCar <zahra.boroujeni@fu-berlin.de>
2017-01-30 11:38:34 +0100

    Update copy.sh

--- copy.sh
+++ copy.sh
@@ -1,10 +1,10 @@
-sudo scp -r catkin_ws root@192.168.1.199:./
-sudo scp -r extra_library/glfw root@192.168.1.199:./
-sudo scp -r extra_library/librealsense root@192.168.1.199:./
-sudo scp autostart.sh root@192.168.1.199:./
-sudo scp .bashrc root@192.168.1.199:./
-sudo scp installation_file/set.sh root@192.168.1.199:./
-sudo scp installation_file/set2.sh root@192.168.1.199:./
+scp -r catkin_ws root@192.168.1.199:./
+scp -r extra_library/glfw root@192.168.1.199:./
+scp -r extra_library/librealsense root@192.168.1.199:./
+scp autostart.sh root@192.168.1.199:./
+scp .bashrc root@192.168.1.199:./
+scp installation_file/set.sh root@192.168.1.199:./
+scp installation_file/set2.sh root@192.168.1.199:./
 
 echo "copy finished!"
 
