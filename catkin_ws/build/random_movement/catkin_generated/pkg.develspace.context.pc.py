0d073b9a57593688565c9c37ccf39faa6af4da88
zahraboroujeni <zahra.boroujeni@gmail.com>
2017-07-28 14:29:58 +0200

    clean build

--- catkin_ws/build/random_movement/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;sensor_msgs;geometry_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "random_movement"
-PROJECT_SPACE_DIR = "/root/catkin_ws/devel/.private/random_movement"
-PROJECT_VERSION = "0.0.0"
--- catkin_ws/build/serial/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/catkin_ws/src/serial
-# Build directory: /root/catkin_ws/build/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(tests)
