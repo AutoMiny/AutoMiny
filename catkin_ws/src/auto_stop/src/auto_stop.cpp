688371910608e63136dc6b3ff990c3a8bd32b2fd
D. Neumann <daniel.neumann@fu-berlin.de>
2018-01-18 15:27:54 +0100

    auto_stop: fix parameter handling

--- catkin_ws/src/auto_stop/src/auto_stop.cpp
+++ catkin_ws/src/auto_stop/src/auto_stop.cpp
@@ -1,22 +1,19 @@
-#include "ros/ros.h"
-#include "sensor_msgs/LaserScan.h"
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
 #include <std_msgs/Int16.h>
 #include <geometry_msgs/Twist.h>
-
-//parameters
-#include <ros/ros.h>
 
 class auto_stop
 {
 public:
-	auto_stop(ros::NodeHandle nh)
+	auto_stop(ros::NodeHandle nh, ros::NodeHandle nh_)
 	{
-		n_.param<int>("angle_front", angle_front, 40);
-		n_.param<int>("angle_back", angle_back, 40);
-		n_.param<float>("break_distance", break_distance, 0.45);
-		pubEmergencyStop_=nh.advertise<std_msgs::Int16>(nh.resolveName("manual_control/speed"), 1);
-		subScan_ = n_.subscribe("scan", 1, &auto_stop::scanCallback,this);
-		subTwist_ = n_.subscribe("model_car/twist",1,&auto_stop::speedCallback,this); 
+		nh_.param<int>("angle_front", angle_front, 40);
+		nh_.param<int>("angle_back", angle_back, 40);
+		nh_.param<float>("break_distance", break_distance, 0.45);
+		pubEmergencyStop_ = nh.advertise<std_msgs::Int16>(nh.resolveName("manual_control/speed"), 1);
+		subScan_ = nh_.subscribe("scan", 1, &auto_stop::scanCallback,this);
+		subTwist_ = nh_.subscribe("motor_control/twist",1,&auto_stop::speedCallback,this);
 	}
 	~auto_stop(){}
 
@@ -28,16 +25,15 @@ public:
 	void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 	{
 	    int count = scan->scan_time / scan->time_increment;
-	    float  break_distance_=break_distance;
+	    float break_distance_=break_distance;
 	    if (abs(direction)>50)
 	    	break_distance_=(abs(direction)/50)*break_distance;
 	    std_msgs::Int16 speed;
 	    speed.data=0;
-	    //ROS_INFO("speed %f",break_distance_);	
+	    //ROS_INFO("speed %f",break_distance_);
 		if(direction > 0){	//backw.
 			for(int i = 0; i < (angle_back/2)+1; i++){
 				if (scan->ranges[i] <= break_distance_){
-				
 					pubEmergencyStop_.publish(speed);
 					//ROS_INFO("Obstacle");
 					return;
@@ -66,7 +62,6 @@ public:
 		int angle_back;
 		float break_distance;
 		int direction;
-	  	ros::NodeHandle n_; 
 	  	ros::Publisher pubEmergencyStop_;
 	  	ros::Subscriber subScan_;
 	  	ros::Subscriber subTwist_;
@@ -78,8 +73,9 @@ public:
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "auto_stop_node");
-    ros::NodeHandle nh; 
-    auto_stop autoStopObject(nh);
+    ros::NodeHandle nh;
+    ros::NodeHandle nh_("~");
+    auto_stop autoStopObject(nh, nh_);
 
 	while(ros::ok())
 	{
