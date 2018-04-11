70c701e8623cd7743c88adde67bc8c8491d7b03a
boroujeni <boroujeni@mi.fu-berlin.de>
2018-04-11 14:49:34 +0200

    add two lines to at the top

--- catkin_ws/src/fub_steering_calibration/scripts/angle_calibrator_online.py
+++ catkin_ws/src/fub_steering_calibration/scripts/angle_calibrator_online.py
@@ -4,7 +4,6 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import namedtuple
-from decimal import Decimal 
 
 from scipy import stats
 
--- catkin_ws/src/fub_steering_calibration/scripts/calibration.sh
+++ catkin_ws/src/fub_steering_calibration/scripts/calibration.sh
@@ -12,5 +12,5 @@ echo "Desired command angle= " $i
 python angle_calibrator_online.py $i
 
 done
-
+sed -i '1i<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>\n<!DOCTYPE boost_serialization>' SteerAngleActuator.xml
 scp SteerAngleActuator.xml root@192.168.43.111:./catkin_ws/src/odometry/cfg/
