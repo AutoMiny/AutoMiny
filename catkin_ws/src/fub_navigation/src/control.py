5efd57a972e1e04f92911414b04ac687b47ca1f1
boroujeni <boroujeni@mi.fu-berlin.de>
2018-05-15 18:41:55 +0200

    stop while press ctrl C

--- catkin_ws/src/fub_navigation/src/control.py
+++ catkin_ws/src/fub_navigation/src/control.py
@@ -3,16 +3,21 @@ import numpy as np
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped, PointStamped
+from fub_navigation.srv import *
 
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Int16, UInt8, Float32
+import rospkg
 
 
-class ForceController:
+class VectorfieldController:
     def __init__(self):
+        rospy.init_node('VectorfieldController')
+        self.pub_speed = rospy.Publisher("/manual_control/speed", Int16, queue_size=100, latch=True)
+        rospy.on_shutdown(self.shutdown)
+
         self.shutdown_=False
         self.pub = rospy.Publisher("/steering", UInt8, queue_size=1)
-        self.pub_speed = rospy.Publisher("/manual_control/speed", Int16, queue_size=100, latch=True)
         self.pub_yaw = rospy.Publisher("/desired_yaw", Float32, queue_size=100, latch=True)
 
         #self.sub_yaw = rospy.Subscriber("/model_car/yaw", Float32, self.callback, queue_size=1)
@@ -27,21 +32,20 @@ class ForceController:
         self.resolution = 10 # cm
         self.lane=1
         self.speed_value= 300
+        rospack = rospkg.RosPack()
+        file_path=rospack.get_path('fub_navigation')+'/src/'
         if (self.lane==1):
-        	self.matrix = np.load('matrix50cm_lane1.npy')
+        	self.matrix = np.load(file_path+'matrix50cm_lane1.npy')
         else:
-        	self.matrix = np.load('matrix100cm_lane2.npy')
-    def __del__(self):
-        print("died")
-        self.shutdown_=True
+        	self.matrix = np.load(file_path+'matrix100cm_lane2.npy')
 
     def lane_callback(self, data):
     	if (self.lane==1):
     		self.lane=2
-    		self.matrix = np.load('matrix100cm_lane2.npy')
+    		self.matrix = np.load(file_path+'matrix100cm_lane2.npy')
     	else:
     		self.lane=1
-    		self.matrix = np.load('matrix100cm_lane1.npy')
+    		self.matrix = np.load(file_path+'matrix100cm_lane1.npy')
 
     def callback(self, data):
         x = data.pose.pose.position.x
@@ -65,10 +69,9 @@ class ForceController:
 
         x3, y3 = self.matrix[x_index,y_index,:]
         f_x=np.cos(yaw)*x3 + np.sin(yaw)*y3
-        print(f_x)
 
         f_y=-np.sin(yaw)*x3 + np.cos(yaw)*y3
-        Kp=-4.0
+        Kp=4.0
         steering=Kp*np.arctan(f_y/(2.5*f_x))
         yaw = np.arctan(f_y/(f_x))
         self.pub_yaw.publish(Float32(yaw))
@@ -78,9 +81,9 @@ class ForceController:
         else:
             speed = -self.speed_value
             if (f_y>0):
-            	steering = np.pi/2
-            if (f_y<0):
             	steering = -np.pi/2
+            if (f_y<0):
+            	steering = np.pi/2
 
         if (steering>(np.pi)/2):
             steering = (np.pi)/2
@@ -92,24 +95,24 @@ class ForceController:
 
 
         steering = 90 + steering * (180/np.pi)
+        # print(steering)
+
         self.pub.publish(UInt8(steering))
         if not self.shutdown_:
             self.pub_speed.publish(Int16(speed))
 
-
-def log(name, var):
-    print '%s:\n%s\n' % (name, var)
-    
+    def shutdown(self):
+        print("shutdown!")
+        self.shutdown_=True
+        self.pub_speed.publish(Int16(0))
+        rospy.sleep(1)
 
 def main():
-    global pub_speed_command
-    rospy.init_node('ForceController')
-    class1=ForceController()  # constructor creates publishers / subscribers
-    while not rospy.is_shutdown():
+    try:
+        VectorfieldController() 
         rospy.spin()
-    else:
-        class1.__del__()
-
+    except rospy.ROSInterruptException:
+        rospy.loginfo("VectorfieldController node terminated.")
 
 
 if __name__ == '__main__':
