252347c855381f9299549b1f0a7affcddb960e94
Zahra Boroujeni <boroujeni@kawasaki.imp.fu-berlin.de>
2018-08-06 17:41:37 +0200

    more precise middle points

--- catkin_ws/src/fub_localization/new_map/new_map.py
+++ catkin_ws/src/fub_localization/new_map/new_map.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure()
 resolution =0.01
-counter_res=1
 Width=3.85
 Length=5.67
 R= [1.5,1.1675,0.835]
@@ -72,91 +71,54 @@ def save(csv_name,loop):
 def save_middle(csv_name,loop):
     global resolution, Width, Length, R
     dis_edge=(Width/2.0-R[0])
-    Broken_line_length=[36,45]
-
     x = []
     y=[]
-    counter =0
-    empty_space=False
-    for i in frange(4.12,R[0],resolution):
-        if (counter < Broken_line_length[0] and empty_space==False):
-            yp=dis_edge+(R[0]-R[loop])
-            xp=i
-            x.append(xp)
-            y.append(yp)
-        else:
-            if (empty_space==False):
-                print(xp, yp)
-                empty_space=True
-                counter=0
-            else:
-                if (counter+1>=Broken_line_length[1]):
-                    empty_space=False
-                    counter=0
-        counter=counter+counter_res
-    xo=R[0]+more_inside
+
+    for i in frange(4.12,R[0]-resolution,resolution):
+        yp=dis_edge + (R[0]-R[loop])
+        xp=i
+        x.append(xp)
+        y.append(yp)
+    xo=R[0]
     yo=R[0]+dis_edge
-    counter=counter-counter_res
     for i in frange(3*np.pi/2.0,np.pi/2.0,resolution/R[loop]):
-        if (counter <= Broken_line_length[0] and empty_space==False):
-            xp = xo + R[loop] * np.cos(i)
-            yp = yo + R[loop] * np.sin(i)
-            y.append(yp)
-            x.append(xp)
-        else:
-            if (empty_space==False):
-                empty_space=True
-                counter=0
-                print(xp, yp)
-            else:
-                if (counter+1>=Broken_line_length[1]):
-                    empty_space=False
-                    counter=0
-        counter=counter+counter_res
-    for i in frange(R[0],Length-R[0]+resolution,resolution):
-        counter=counter+counter_res
-        if (counter < Broken_line_length[0] and empty_space==False):
-            xp=i
-            yp=dis_edge+R[0]+R[loop]
-            x.append(i)
-            y.append(yp)
+        xp = xo + R[loop] * np.cos(i)
+        yp = yo + R[loop] * np.sin(i)
+        y.append(yp)
+        x.append(xp)
 
-        else:
-            if (empty_space==False):
-                empty_space=True
-                counter=0
-                print(xp, yp)
-            else:
-                if (counter+1>=Broken_line_length[1]):
-                    empty_space=False
-                    counter=0
-    xo=Length-R[0]-more_inside
+    for i in frange(R[0],Length-R[0]+resolution,resolution):          
+        xp=i
+        yp=dis_edge+R[0]+R[loop]
+        x.append(i)
+        y.append(yp)
+    xo=Length-R[0]
     yo=R[0]+dis_edge
-    counter=counter-counter_res
-    for i in frange(np.pi/2.0,-np.pi/2.0-resolution/R[loop],resolution/R[loop]):
-        counter=counter+counter_res
-        if (counter <= Broken_line_length[0] and empty_space==False):
-            xp = xo + R[loop] * np.cos(i)
-            yp = yo + R[loop] * np.sin(i)
-            y.append(yp)
-            x.append(xp)
+    for i in frange(np.pi/2.0,-np.pi/2.0,resolution/R[loop]):
+        xp = xo + R[loop] * np.cos(i)
+        yp = yo + R[loop] * np.sin(i)
+        y.append(yp)
+        x.append(xp)
 
-        else:
-            if (empty_space==False):
-                print(xp, yp)
-                empty_space=True
-                counter=0
-            else:
-                if (counter+1>=Broken_line_length[1]):
-                    empty_space=False
-                    counter=0
     plt.hold(True)
     name=csv_name
     file = open(name,"w")
+    counter=0
+    empty=False
     for i in range(len(x)):
-        file.write('1.' + str(format(i+1))+ '\t' + str("{:.3f}".format(x[i]))+ '\t'+str("{:.3f}".format(y[i]))+'\n')
-        plt.hold(True)
-        plt.plot(x[i],y[i],':o')
+        if (counter<35):
+            file.write('1.' + str(format(i+1))+ '\t' + str("{:.3f}".format(x[i]))+ '\t'+str("{:.3f}".format(y[i]))+'\n')
+            plt.hold(True)
+            plt.plot(x[i],y[i],':o')    
+        else:
+            if (empty==False):
+                print(x[i-1],y[i-1])
+                empty=True
+            if (counter>=80):
+                counter=-1
+                empty=False
+        counter=counter+1
+
     file.close()
 
 
