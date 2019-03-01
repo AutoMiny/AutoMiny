8ebac29c14f63608aee5c26ff02117bcace4c578
Stephan Sundermann <stephansundermann@gmail.com>
2019-03-01 13:27:28 +0100

    Add pose estimation offsets

--- catkin_ws/src/stereo_camera_pose_estimation/src/StereoCameraPoseEstimation.cpp
+++ catkin_ws/src/stereo_camera_pose_estimation/src/StereoCameraPoseEstimation.cpp
@@ -140,13 +140,11 @@ namespace stereo_camera_pose_estimation {
                         (cameraTransformRotation(0, 0), cameraTransformRotation(0, 1), cameraTransformRotation(0, 2),
                          cameraTransformRotation(1, 0), cameraTransformRotation(1, 1), cameraTransformRotation(1, 2),
                          cameraTransformRotation(2, 0), cameraTransformRotation(2, 1), cameraTransformRotation(2, 2));
-                tf::Vector3 translation(tvecs[i][0], tvecs[i][1], tvecs[i][2]);
 
                 tf::Quaternion q;
                 rotation.getRotation(q);
                 q = t * q;
                 q = q.inverse();
-                translation = t * translation;
 
                 yaw = tf::getYaw(q);
 
@@ -172,8 +170,8 @@ namespace stereo_camera_pose_estimation {
             return false;
         }
 
-        auto rot = tf::createQuaternionFromRPY(roll, -pitch, yaw);
-        auto trans = tf::Vector3(0.0, 0.0, height);
+        auto rot = tf::createQuaternionFromRPY(roll + config.roll_offset, -pitch + config.pitch_offset, yaw + config.yaw_offset);
+        auto trans = tf::Vector3(0.0 + config.x_offset, 0.0 + config.y_offset, height + config.height_offset);
         tf::Transform t(rot, trans);
         tf::StampedTransform transform(t, ros::Time::now(), "base_link", "camera_link");
         tfBroadcaster.sendTransform(transform);
