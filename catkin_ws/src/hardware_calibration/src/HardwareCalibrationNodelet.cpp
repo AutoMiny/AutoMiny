6039ce34ab6fcc16e95761d28d83b23c06e6b898
Stephan Sundermann <stephansundermann@gmail.com>
2019-07-04 08:38:12 +0200

    Make clipping simpler

--- catkin_ws/src/hardware_calibration/src/HardwareCalibrationNodelet.cpp
+++ catkin_ws/src/hardware_calibration/src/HardwareCalibrationNodelet.cpp
@@ -90,10 +90,7 @@ namespace hardware_calibration {
                     -  3.3170433193228730e+000 * std::pow(x, 3);
 
             // interpolation is not perfect so clip
-            if (msg->value > 0 && mps < 0) {
-                mps = 0;
-            }
-            if (msg->value < 0 && mps > 0) {
+            if (mps < 0) {
                 mps = 0;
             }
 
@@ -142,10 +139,7 @@ namespace hardware_calibration {
                                +  2.3646519448237449e-001 * pow(x,3);
 
             // interpolation is not perfect so clip
-            if (msg->value > 0 && normalized < 0) {
-                normalized = 0;
-            }
-            if (msg->value < 0 && normalized > 0) {
+            if (normalized < 0) {
                 normalized = 0;
             }
 
