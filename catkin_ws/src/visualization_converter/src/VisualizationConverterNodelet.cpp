b2a3d8b2c48c439ed398b6a932392499542d05a3
Stephan Sundermann <stephansundermann@gmail.com>
2019-07-09 16:39:56 +0200

    Initialize publishers first

--- catkin_ws/src/visualization_converter/src/VisualizationConverterNodelet.cpp
+++ catkin_ws/src/visualization_converter/src/VisualizationConverterNodelet.cpp
@@ -6,19 +6,19 @@ namespace visualization_converter {
         ros::NodeHandle nh = getNodeHandle();
         ros::NodeHandle pnh = getPrivateNodeHandle();
 
-        /// Subscriber
-        steeringAngleSubscriber = pnh.subscribe("sensors/steering", 10, &VisualizationConverterNodelet::onSteeringAngle, this);
-        speedSubscriber = pnh.subscribe("sensors/speed", 10, &VisualizationConverterNodelet::onSpeed, this);
-        wantedSpeedSubscriber = pnh.subscribe("actuators/speed", 10, &VisualizationConverterNodelet::onWantedSpeed, this);
-        wantedSteeringSubscriber = pnh.subscribe("actuators/steering", 10, &VisualizationConverterNodelet::onWantedSteering, this);
-        voltageSubscriber = pnh.subscribe("sensors/voltage", 10, &VisualizationConverterNodelet::onVoltage, this);
-
         /// Publisher
         steeringAnglePublisher = pnh.advertise<autominy_msgs::Plot>("visualization/steering_angle", 1);
         speedPublisher = pnh.advertise<autominy_msgs::Plot>("visualization/speed", 1);
         wantedSpeedPublisher = pnh.advertise<autominy_msgs::Plot>("visualization/wanted_speed", 1);
         wantedSteeringPublisher = pnh.advertise<autominy_msgs::Plot>("visualization/wanted_steering", 1);
         voltagePublisher = pnh.advertise<autominy_msgs::Plot>("visualization/voltage", 1);
+
+        /// Subscriber
+        steeringAngleSubscriber = pnh.subscribe("sensors/steering", 10, &VisualizationConverterNodelet::onSteeringAngle, this);
+        speedSubscriber = pnh.subscribe("sensors/speed", 10, &VisualizationConverterNodelet::onSpeed, this);
+        wantedSpeedSubscriber = pnh.subscribe("actuators/speed", 10, &VisualizationConverterNodelet::onWantedSpeed, this);
+        wantedSteeringSubscriber = pnh.subscribe("actuators/steering", 10, &VisualizationConverterNodelet::onWantedSteering, this);
+        voltageSubscriber = pnh.subscribe("sensors/voltage", 10, &VisualizationConverterNodelet::onVoltage, this);
     }
 
     void VisualizationConverterNodelet::onSpeed(const autominy_msgs::SpeedConstPtr& msg) {
